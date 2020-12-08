fdf538fdb1580420fc0eab155ce4f6f108be1170
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2020-12-09 00:47:36 +0800

    Update push_message.json.js

--- push_message.json.js
+++ push_message.json.js
@@ -1,3 +1,3 @@
 {
-"content2":"<marquee><em style='color: gold;'>JC：</em>&nbsp;&nbsp;<a href='https://poicloud.icu/' target='_blank'>【poicloud】</a></marquee>"
+"content2":"<marquee><em style='color: gold;'>木几土昜：</em>&nbsp;&nbsp;<a href='https://poicloud.icu/' target='_blank'>【poicloud】</a></marquee>"
 }

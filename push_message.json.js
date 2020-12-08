832471b6f637fb9455d74411c4c65fce35a32f36
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2020-12-08 17:20:36 +0800

    Update push_message.json.js

--- push_message.json.js
+++ push_message.json.js
@@ -3,6 +3,7 @@
 <a href='https://github.com/shadowsocks/shadowsocks-libev' target='_blank'><em><u>SS</u></em></a>、\
 <a href='https://github.com/shadowsocksrr/shadowsocksr-libev' target='_blank'><em><u>SSR</u></em></a>、\
 <a href='https://github.com/v2ray/v2ray-core' target='_blank'><em><u>V2Ray</u></em></a>、\
-<a href='https://github.com/trojan-gfw/trojan' target='_blank'><em><u>Trojan</u></em></a>等方式科学上网。",
+<a href='https://github.com/trojan-gfw/trojan' target='_blank'><em><u>Trojan</u></em></a>\
+等方式科学上网。",
 "content2":"<em>恰饭用</em><a href='https://github.com/Dreamacro/clash' target='_blank'>Clash</a>"
 }

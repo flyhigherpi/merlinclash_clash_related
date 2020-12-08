47bc6a856ebf4e49ba99eabc62fd3e68a93d255b
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2020-12-08 17:37:27 +0800

    TEST

--- push_message.json.js
+++ push_message.json.js
@@ -1,3 +1,3 @@
 {
-"content2":"<em style='color:gold'>机场推荐</em>&nbsp;&nbsp;<a href='https://github.com/Dreamacro/clash' target='_blank'>Clash</a>"
+"content2":"<em style='color: gold;'>机场推荐</em>&nbsp;&nbsp;<a href='https://github.com/Dreamacro/clash' target='_blank'>Clash</a>"
 }

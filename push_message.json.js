3d43c94ba9e6bac3100567418fcc0a7857f83a93
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2020-12-08 17:25:46 +0800

    Update push_message.json.js

--- push_message.json.js
+++ push_message.json.js
@@ -1,9 +1,3 @@
 {
-"content1":"<a href='https://github.com/Dreamacro/clash' target='_blank'><em><u>Clash</u></em></a>是一个基于规则的代理程序，支持\
-<a href='https://github.com/shadowsocks/shadowsocks-libev' target='_blank'><em><u>SS</u></em></a>、\
-<a href='https://github.com/shadowsocksrr/shadowsocksr-libev' target='_blank'><em><u>SSR</u></em></a>、\
-<a href='https://github.com/v2ray/v2ray-core' target='_blank'><em><u>V2Ray</u></em></a>、\
-<a href='https://github.com/trojan-gfw/trojan' target='_blank'><em><u>Trojan</u></em></a>\
-等方式科学上网。",
 "content2":"<em>恰饭用</em><a href='https://github.com/Dreamacro/clash' target='_blank'>Clash</a>"
 }

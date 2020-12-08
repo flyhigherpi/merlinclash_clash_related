2628605ca594d46bddda3a3a60d8d4ecba2de85a
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2020-12-09 00:13:36 +0800

    TE

--- push_message.json.js
+++ push_message.json.js
@@ -1,3 +1,3 @@
 {
-"content2":"<em style='color: gold;'>恰点饭吃：</em>&nbsp;&nbsp;<a href='https://poicloud.icu/' target='_blank'>【佛系老鸡场 poicloud.icu】</a>"
+"content2":"<em style='color: gold;'>JC：</em>&nbsp;&nbsp;<a href='https://poicloud.icu/' target='_blank'>【poicloud】</a>"
 }

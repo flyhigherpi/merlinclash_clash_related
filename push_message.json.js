c5e6548c53688bb72fd0a4fed183ee41ba0b9e4c
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2020-12-10 00:47:57 +0800

    Y

--- push_message.json.js
+++ push_message.json.js
@@ -1,3 +1,3 @@
 {
-"content2":"<marquee><em style='color: gold;'>木几土昜：</em>&nbsp;&nbsp;<a href='https://poicloud.icu/' target='_blank'>【poicloud】</a></marquee>"
+"content2":"<marquee><em style='color: gold;'>木几土昜【我的阿福】：</em>&nbsp;&nbsp;<a href='https://poicloud.icu/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【poicloud】</a></marquee>"
 }

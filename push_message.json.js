7043d72abe6ea395bec27d379575e9d49c906b26
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2020-12-10 16:35:17 +0800

    Y

--- push_message.json.js
+++ push_message.json.js
@@ -1,4 +1,5 @@
 {
 "content1":"<em style='color: gold;'>公告区：1209999-001补丁安装后没有屏蔽云村解锁等选项请重下补丁安装</em>",
-"content2":"<marquee><em style='color: gold;'>木几土昜【我的阿福】：</em>&nbsp;&nbsp;<a href='https://poicloud.icu/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【poicloud】</a></marquee>"
+"content2":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
+"content3":"<marquee><em style='color: gold;'>木几土昜【我的阿福】：</em>&nbsp;&nbsp;<a href='https://poicloud.icu/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【poicloud】</a></marquee>"
 }

fdd33f4d9753b3072d875100af70291e108dca98
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2020-12-15 23:57:31 +0800

    U

--- push_message.json.js
+++ push_message.json.js
@@ -1,5 +1,5 @@
 {
-"content1":"<em style='color: gold;'>公告区：天冷躲被窝了~~</em>",
+"content1":"<em style='color: gold;'>公告区：小米猫眼等设备如果离线，需要安装1209-003补丁后添加进黑名单/KP兼容设置不通过代理</em>",
 "content2":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
 "content3":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://poicloud.icu/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【PoiCloud--月付便宜、丰俭由人】</a></marquee>"
 }

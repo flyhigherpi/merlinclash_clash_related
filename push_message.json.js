724744995b799525a44b4d747f42c3b066e65da1
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2020-12-28 01:46:03 +0800

    Y

--- push_message.json.js
+++ push_message.json.js
@@ -1,6 +1,6 @@
 {
-"content1":"<em style='color: gold;'>公告区：HND1225已重发，如果订阅不了/无法导出黑白名单，请重下重装</em>",
-"content2":"<em style='color: gold;'>公告区：380-1221已重发，修正保存&应用点击没反应问题</em>",
+"content1":"<em style='color: gold;'>公告区：1225-002补丁新增一键备份/一键还原，建议更新</em>",
+"content2":"<em style='color: gold;'>公告区：380功能同步中，等~~~</em>",
 "content3":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
 "content4":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://poicloud.icu/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【PoiCloud--月付便宜、丰俭由人】</a></marquee>"
 }

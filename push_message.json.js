8c4312450a1fd01d59da3c250a5c0b5aea3e623e
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2020-12-29 21:56:00 +0800

    U

--- push_message.json.js
+++ push_message.json.js
@@ -1,5 +1,5 @@
 {
-"content1":"<em style='color: gold;'>公告区：1225-002补丁新增一键备份/一键还原，建议更新</em>",
+"content1":"<em style='color: gold;'>公告区：豆瓣APP拦截，临时解决方法。请在自定规则添加类型：DOMAIN-SUFFIX，内容：erebor.douban.com，方式：DIRECT这条规则</em>",
 "content2":"<em style='color: gold;'>公告区：3801225已发布</em>",
 "content3":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
 "content4":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://poicloud.icu/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【PoiCloud--月付便宜、丰俭由人】</a></marquee>"

70c58dcbebe2359cde1efd92c1d0d39cd50c20b6
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-01-06 00:35:12 +0800

    Y

--- push_message.json.js
+++ push_message.json.js
@@ -1,6 +1,6 @@
 {
 "content1":"<em style='color: gold;'>公告区：豆瓣APP拦截问题，使用MC_规则，HND更新122902SC规则后重新订阅，384用ACL4SSR重新订阅。",
-"content2":"<em style='color: gold;'>公告区：3801225已发布</em>",
+"content2":"<em style='color: gold;font-size:20px'>公告区：1225更新005补丁后，订阅功能请在【附加功能】-【功能显示开关】手动打开。</em>",
 "content3":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
 "content4":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://poicloud.icu/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【PoiCloud--月付便宜、丰俭由人】</a></marquee>"
 }

701288b15541059b085a20f5720a5645c0b5aa5a
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-01-23 01:05:15 +0800

    U

--- push_message.json.js
+++ push_message.json.js
@@ -1,6 +1,6 @@
 {
-"content1":"<em style='color: gold;'>公告区：豆瓣APP拦截问题，使用MC_规则，HND更新122902SC规则后重新订阅，384用ACL4SSR重新订阅。",
-"content2":"<em style='color: gold;'>公告区：出行戴口罩啊。</em>",
+"content1":"<em style='color: gold;'>公告区：IPV6测起来啊。。。",
+"content2":"<em style='color: gold;'>公告区：统计下开启TPROXY-TCP模式无法上网的机型啊。。。</em>",
 "content3":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
 "content4":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://poicloud.icu/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【PoiCloud--月付便宜、丰俭由人】</a></marquee>"
 }

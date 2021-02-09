e707e84732f01bb6321e940d7890085f8b2f2f20
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-02-10 00:22:15 +0800

    U

--- push_message.json.js
+++ push_message.json.js
@@ -1,6 +1,6 @@
 {
 "content1":"<em style='color: gold;'>公告区：IPV6测起来啊!需打开路由IPV6，插件需打开TPROXY-TCP/TCP&UDP和ipv6模式。。。",
-"content2":"<em style='color: gold;'>公告区：0119-009.修复方案二其他设备不通过clash导致全部设备翻墙失败问题</em>",
+"content2":"<em style='color: gold;'>公告区：0209已发。记得备份啊。。。看不到就算了！！！</em>",
 "content3":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
 "content4":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://poicloud.icu/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【PoiCloud--月付便宜、丰俭由人】</a></marquee>"
 }

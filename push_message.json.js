a5df1923077ed07006cd12c6b8996a7c7cfae25c
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-04-08 19:34:56 +0800

    Y

--- push_message.json.js
+++ push_message.json.js
@@ -1,5 +1,5 @@
 {
-"content1":"<em style='color: gold;'>公告区：最新补丁已重发，【又】修正了黑白郎君",
+"content1":"<em style='color: gold;'>公告区：最新补丁已重发，修正了hosts丢失问题",
 "content2":"<em style='color: gold;'>公告区：近期DNS解析问题：<a href='https://t.me/merlinclashcat/294' target='_blank'>DNS设置</a></em>",
 "content3":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
 "content4":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://poicloud.icu/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【PoiCloud--月付便宜、丰俭由人】</a></marquee>"

71d127084f0a7e9ab0412f043f777e07ad864d97
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-04-26 00:54:07 +0800

    U

--- push_message.json.js
+++ push_message.json.js
@@ -1,5 +1,5 @@
 {
-"content1":"<em style='color: gold;'>公告区：云村解锁请下载最新0.2.10二进制使用",
+"content1":"<em style='color: gold;'>公告区：云村解锁请下载最新0.2.10二进制使用，补丁重发，修复云村解锁不显示音源最大搜索结果文本框导致传参出错问题。</em>",
 "content2":"<em style='color: gold;'>公告区：近期DNS解析问题：<a href='https://t.me/merlinclashcat/294' target='_blank'>DNS设置</a></em>",
 "content3":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
 "content4":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://poicloud.icu/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【PoiCloud--月付便宜、丰俭由人】</a></marquee>"

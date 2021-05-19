6ebfae92c46544c2bf7ad3b2f7895698111fbbf4
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-05-19 11:05:04 +0800

    Y

--- push_message.json.js
+++ push_message.json.js
@@ -1,6 +1,6 @@
 {
 "content1":"<em style='color: gold;'>公告区：云村解锁请下载最新0.2.10二进制使用，补丁重发，修复云村解锁不显示音源最大搜索结果文本框导致传参出错问题。</em>",
-"content2":"<em style='color: gold;'>公告区：近期DNS解析问题：<a href='https://t.me/merlinclashcat/294' target='_blank'>DNS设置</a></em>",
+"content2":"<em style='color: gold;'>【推】华为智慧屏代理（价钱优势，全国可发）--石头哥，QQ：20052795。好友暗号：【满地星光】</em>",
 "content3":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
 "content4":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://poicloud.icu/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【PoiCloud--月付便宜、丰俭由人】</a></marquee>"
 }

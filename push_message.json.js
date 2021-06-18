0a39b9e3bc9058637c7b0bc14588f1f44be3faee
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-06-19 01:25:21 +0800

    Y

--- push_message.json.js
+++ push_message.json.js
@@ -1,6 +1,5 @@
 {
 "content1":"<em style='color: gold;'>公告区：云村解锁请下载最新0.2.10二进制使用，补丁重发，修复云村解锁不显示音源最大搜索结果文本框导致传参出错问题。</em>",
-"content2":"<em style='color: gold;'>【推】华为智慧屏，价钱优势，全国可发，微信号：XtexNs。好友暗号：【满地星光】【PS.俺不卖机场不搞插件T_T】</em>",
-"content3":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
-"content4":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tianshuhl.com/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联(原PoiCloud)--月付便宜、丰俭由人】</a></marquee>"
+"content2":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
+"content3":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tianshuhl.com/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联(PoiCloud)--文案不行吸引不到人怎么办！！！】</a></marquee>"
 }

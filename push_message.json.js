501537751fa6d57d843799ada378e13ceaa0d437
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-07-05 20:58:18 +0800

    U

--- push_message.json.js
+++ push_message.json.js
@@ -1,5 +1,5 @@
 {
-"content1":"<em style='color: gold;'>公告区：云村解锁请下载最新0.2.10二进制使用，补丁重发，修复云村解锁不显示音源最大搜索结果文本框导致传参出错问题。</em>",
+"content1":"<em style='color: gold;'>公告区：【恩山版】MerlinClash已更新0705版本，几乎修复全功能使用了。</em>",
 "content2":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
 "content3":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tianshuhl.com/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联(PoiCloud)--文案不行吸引不到人怎么办！！！】</a></marquee>"
 }

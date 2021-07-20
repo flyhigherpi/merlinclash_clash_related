04ec0a17e71cdf2f9175cb6adc6f26c037489dbd
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-07-21 02:04:30 +0800

    U

--- push_message.json.js
+++ push_message.json.js
@@ -1,5 +1,5 @@
 {
 "content1":"<em style='color: gold;'>公告区：【恩山版】MerlinClash已更新0705版本，几乎修复全功能使用了。</em>",
 "content2":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
-"content3":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tianshuhl.com/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联(PoiCloud)--文案不行吸引不到人怎么办！！！】</a></marquee>"
+"content3":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tianshuhl.com/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联--文案不行吸引不到人怎么办！！！】</a></marquee>"
 }

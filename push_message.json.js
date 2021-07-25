25e34648e2b130c9a59b88a61d3a8c6c0a181949
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-07-25 23:53:17 +0800

    Y

--- push_message.json.js
+++ push_message.json.js
@@ -1,6 +1,6 @@
 {
 "content1":"<em style='color: gold;'>公告区：【恩山版】MerlinClash已更新0705版本，几乎修复全功能使用了。</em>",
-"content2":"<em style='color: gold;'>公告区：【KS版本】更新0722版本后导致页面崩溃或无法启动，解决方法如下：ssh下依次输入1：dbus set merlinclash_links＝\" \" 2：dbus set merlinclash_links2＝\" \" 3：dbus set merlinclash_links3＝\" \"</em>",
+"content2":"<em style='color: gold;'>公告区：【KS版本】更新0722版本后还原备份导致页面崩溃或无法启动，解决方法见<a href='https://t.me/merlinclashcat/39' target='_blank'>【FAQ14】</a></em>",
 "content3":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
 "content4":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tianshuhl.com/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联--文案不行吸引不到人怎么办！！！】</a></marquee>"
 }

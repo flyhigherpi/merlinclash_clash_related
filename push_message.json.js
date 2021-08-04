69ae5f6ba8f461bf86a5f4e64c829e3bd8fe4138
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-08-04 12:20:49 +0800

    Y

--- push_message.json.js
+++ push_message.json.js
@@ -1,5 +1,5 @@
 {
 "content1":"<em style='color: gold;'>公告区：【恩山版】MerlinClash已更新0705版本，几乎修复全功能使用了。</em>",
 "content2":"<em style='color: gold;'>公告区：【KS版本】更新0722版本后还原备份导致页面崩溃或无法启动，解决方法见<a href='https://t.me/merlinclashcat/39' target='_blank'>【FAQ14】</a></em>",
-"content3":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
+"content3":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>"
 }

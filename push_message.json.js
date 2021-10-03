7aa53b15cfe9e45fe7c11fcbf9bee256eed1b982
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-10-03 11:00:18 +0800

    Update push_message.json.js

--- push_message.json.js
+++ push_message.json.js
@@ -1,6 +1,6 @@
 {
 "content1":"<em style='color: gold;'>公告区：【恩山版】MerlinClash已更新0705版本，几乎修复全功能使用了。</em>",
-"content2":"<em style='color: gold;'>公告区：【KS版本】0917-007补丁已出，有IPV6需求必更。在不代理IPV6情况下也能正常访问网站了。</em>",
+"content2":"<em style='color: gold;'>公告区：【KS版本】0917384-002补丁已出，修复订阅无效问题。</em>",
 "content3":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
 "content4":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tianshuhl.com/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】</a></marquee>"
 }

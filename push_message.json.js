f9b6b1b8d3c3e59ce7aebc3bd0e6926356fd904d
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-10-05 00:33:05 +0800

    Y

--- push_message.json.js
+++ push_message.json.js
@@ -1,6 +1,6 @@
 {
 "content1":"<em style='color: gold;'>公告区：【恩山版】MerlinClash已更新0705版本，几乎修复全功能使用了。</em>",
-"content2":"<em style='color: gold;'>公告区：【KS版本】0917384-004补丁已重发，修复host下拉框无值问题。或者直接重下1003PATCH4包重装</em>",
+"content2":"<em style='color: gold;'>公告区：【KS版本】1003-005补丁已重发，增加对HOST区长文件的保存支持</em>",
 "content3":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
 "content4":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tianshuhl.com/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】</a></marquee>"
 }

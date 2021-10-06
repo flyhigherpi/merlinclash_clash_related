78f05a98d6885bfc05d375e0a9513d61a3787fed
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-10-07 01:38:40 +0800

    Y

--- push_message.json.js
+++ push_message.json.js
@@ -1,6 +1,6 @@
 {
 "content1":"<em style='color: gold;'>公告区：【恩山版】MerlinClash已更新0705版本，几乎修复全功能使用了。</em>",
-"content2":"<em style='color: gold;'>公告区：【KS版本】1003-005补丁已重发，增加对HOST区长文件的保存支持</em>",
+"content2":"<em style='color: gold;'>公告区：【KS版本】1007已发，更改了一堆</em>",
 "content3":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
 "content4":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tianshuhl.com/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】</a></marquee>"
 }

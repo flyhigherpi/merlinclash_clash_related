9c9b1fdaba3883644246d53a1c98419d81c80e73
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-10-11 21:09:52 +0800

    Y

--- push_message.json.js
+++ push_message.json.js
@@ -1,6 +1,6 @@
 {
 "content1":"<em style='color: gold;'>公告区：【恩山版】MerlinClash已更新0705版本，几乎修复全功能使用了。</em>",
-"content2":"<em style='color: gold;'>公告区：【KS版本】1007380已重发</em>",
+"content2":"<em style='color: gold;'>公告区：【KS版本】1007-004已重发发，PPTP回城代理出墙好了。</em>",
 "content3":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
 "content4":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tianshuhl.com/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】</a></marquee>"
 }

8ab42a0f067e0ec993fcf109c6041f2acb032ec9
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-10-15 10:49:21 +0800

    Y

--- push_message.json.js
+++ push_message.json.js
@@ -1,5 +1,5 @@
 {
-"content1":"<em style='color: gold;'>公告区：【恩山版】MerlinClash已更新0705版本，几乎修复全功能使用了。</em>",
+"content1":"<em style='color: gold;'>公告区：【恩山版】MerlinClash已更新1007版。</em>",
 "content2":"<em style='color: gold;'>公告区：【KS版本】56V2访问面板提交失败，检查谷歌浏览器chrome://flags/#block-insecure-private-network-requests并关闭</em>",
 "content3":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
 "content4":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tianshuhl.com/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】</a></marquee>"

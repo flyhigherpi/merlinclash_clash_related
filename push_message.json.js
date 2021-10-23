f5c51d140e26758188df13c46406a875e1823d4c
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-10-24 01:45:16 +0800

    Y

--- push_message.json.js
+++ push_message.json.js
@@ -1,6 +1,7 @@
 {
 "content1":"<em style='color: gold;'>公告区：【KS版】【恩山版本】005补丁 006补丁 002补丁 已分别重发</em>",
-"content2":"<em style='color: gold;'>公告区：【KS版本】56V2访问面板提交失败，检查谷歌浏览器chrome://flags/#block-insecure-private-network-requests并关闭</em>",
-"content3":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
-"content4":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tianshuhl.com/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】</a></marquee>"
+"content2":"<em style='color: gold;'>公告区：【KS版】007补丁已发，修正DNS回环问题</em>",
+"content3":"<em style='color: gold;'>公告区：【KS版本】56V2访问面板提交失败，检查谷歌浏览器chrome://flags/#block-insecure-private-network-requests并关闭</em>",
+"content4":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
+"content5":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tianshuhl.com/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】</a></marquee>"
 }

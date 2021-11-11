1d6e20c0f460e18affe68ee0e17749b58be868e4
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-11-11 23:49:43 +0800

    Y

--- push_message.json.js
+++ push_message.json.js
@@ -1,5 +1,5 @@
 {
-"content1":"<em style='color: gold;'>公告区：【KS版】【恩山版本】005补丁 006补丁 002补丁 已分别重发</em>",
+"content1":"<em style='color: gold;'>公告区：订阅规则已更新，MC规则修复ip查询不显示国外ip，修复google ip格式错误，SC本地转换请更新规则后重新订阅；ACL4SSR转换则重新订阅即可</em>",
 "content2":"<em style='color: gold;'>公告区：【KS版】007补丁已发，修正DNS回环问题</em>",
 "content3":"<em style='color: gold;'>公告区：【KS版本】56V2访问面板提交失败，检查谷歌浏览器chrome://flags/#block-insecure-private-network-requests并关闭</em>",
 "content4":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",

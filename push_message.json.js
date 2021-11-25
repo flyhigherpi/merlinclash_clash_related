abbd41398bef56430374ffbfc3e79607fdd6db44
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-11-25 12:41:43 +0800

    Update push_message.json.js

--- push_message.json.js
+++ push_message.json.js
@@ -1,6 +1,6 @@
 {
-"content1":"<em style='color: gold;'>公告区：订阅规则已更新，MC规则修复ip查询不显示国外ip，修复google ip格式错误，SC本地转换请更新规则后重新订阅；ACL4SSR转换则重新订阅即可</em>",
-"content2":"<em style='color: gold;'>公告区：【KS版】007补丁已发，修正DNS回环问题</em>",
+"content1":"<em style='color: gold;'>公告区：<a href='https://mcreadme.gitbook.io/mc/' target='_blank'>MC帮助文档</a></em>",
+"content2":"<em style='color: gold;'>公告区：【KS版】009补丁已发，更换ip检测接口为ip.sb</em>",
 "content3":"<em style='color: gold;'>公告区：【KS版本】56V2访问面板提交失败，检查谷歌浏览器chrome://flags/#block-insecure-private-network-requests并关闭</em>",
 "content4":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
 "content5":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tianshuhl.com/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】</a></marquee>"

2816b38bbbd545eb02be166d66d73c5e4ae6b847
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-12-31 22:40:11 +0800

    y

--- push_message.json.js
+++ push_message.json.js
@@ -1,7 +1,7 @@
 {
-"content1":"<em style='color: gold;'>公告区：</em>",
+"content1":"<em style='color: gold;'>公告区：2022元旦快乐</em>",
 "content2":"<em style='color: gold;'>公告区：【KS版】迈进2022</em>",
-"content3":"<em style='color: gold;'>公告区：【KS版本】56V2访问面板提交失败，检查谷歌浏览器chrome://flags/#block-insecure-private-network-requests并关闭</em>",
+"content3":"<em style='color: gold;'>公告区：【KS版本】访问管理面板提交失败，检查谷歌浏览器chrome://flags/#block-insecure-private-network-requests并关闭</em>",
 "content4":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='https://mcreadme.gitbook.io/mc/' target='_blank'>MC帮助文档</a></em>",
 "content5":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tianshuhl.com/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】 优惠码: 元旦快乐 </a></marquee>"
 }

6c9430c4d47e3100ed274daa12823936f4067b72
vj23456 <53201527+vj23456@users.noreply.github.com>
2022-03-10 16:06:17 +0800

    Update push_message.json.js

--- push_message.json.js
+++ push_message.json.js
@@ -1,7 +1,7 @@
 {
 "content1":"<em style='color: gold;'>公告区：【KC版本】<a href='https://t.me/merlinclashcat/432' target='_blank'>22.02.28-002补丁</a>已发，建议及时更新</em>",
 "content2":"<em style='color: gold;'>公告区：若Chrome访问YACD面板登录失败，请将chrome://flags/#block-insecure-private-network-requests设置关闭(disable)</em>",
-"content3":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>MerlinClash猫咪爬梯</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;帮助文档：<a href='https://mcreadme.gitbook.io/mc/' target='_blank'>MerlinClash Wiki</a></em>",
+"content3":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>MerlinClash猫咪爬梯</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;帮助文档：<a href='https://mcreadme.gitbook.io/mc/' target='_blank'>MerlinClash Wiki</a></em>",
 "content4":"<em style='color: gold;'>奈飞小铺：<a href='https://ihezu.run/wb7hNF' target='_blank'>流媒体影音（Netflix、Disney+、Youtube、HBO等）账号合租一站式服务平台，95折优惠代码：nf325【通用】</a></em>",
 "content5":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tianshuhl.com/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】 优惠码: 新春快乐 </a></marquee>"
 }

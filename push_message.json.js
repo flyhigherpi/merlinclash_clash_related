7e84d2e73e686e19254aaabd424ffaa8236dc647
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2022-04-07 00:18:25 +0800

    y

--- push_message.json.js
+++ push_message.json.js
@@ -3,5 +3,5 @@
 "content2":"<em style='color: gold;'>公告区：【KS版本】访问管理面板提交失败，检查谷歌浏览器chrome://flags/#block-insecure-private-network-requests并关闭</em>",
 "content3":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='https://mcreadme.gitbook.io/mc/' target='_blank'>MC帮助文档</a></em>",
 "content4":"<em style='color: gold;'>奈飞小铺：<a href='https://ihezu.run/wb7hNF' target='_blank'>流媒体影音（Netflix、Disney+、Youtube、HBO等）账号合租一站式服务平台，95折优惠代码：nf325【通用】</a></em>",
-"content5":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tianshuhl.com/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】 优惠码: 新春快乐 </a></marquee>"
+"content5":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tianshuhl.com/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】</a></marquee>"
 }

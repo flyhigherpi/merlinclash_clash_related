c46ead8bade5e19392bcf5c55ec222d115e50196
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2022-06-29 01:21:24 +0800

    Y

--- push_message.json.js
+++ push_message.json.js
@@ -3,5 +3,5 @@
 "content2":"<em style='color: gold;'>公告区：若Chrome访问YACD面板登录失败，请将chrome://flags/#block-insecure-private-network-requests设置关闭(disable)</em>",
 "content3":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>MerlinClash猫咪爬梯</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;帮助文档：<a href='https://mcreadme.gitbook.io/mc/' target='_blank'>MerlinClash Wiki</a></em>",
 "content4":"<em style='color: gold;'>奈飞小铺：<a href='https://ihezu.run/wb7hNF' target='_blank'>流媒体影音（Netflix、Disney+、Youtube、HBO等）账号合租一站式服务平台，95折优惠代码：nf325【通用】</a></em>",
-"content5":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tianshuhl.com/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】优惠码:Ycx2Vfcg 享年付全场85折优惠 优惠截止至2022-05-15日</a></marquee>"
+"content5":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tianshuhl.com/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】优惠码:7rr4Zt11 享年付全场9折优惠 优惠截止至2022-07-13日</a></marquee>"
 }

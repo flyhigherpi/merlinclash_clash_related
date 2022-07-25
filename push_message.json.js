37af60c86a4a15ab4c72f13334f670acdd8b7eb2
vj23456 <53201527+vj23456@users.noreply.github.com>
2022-07-25 10:54:41 +0800

    Update push_message.json.js

--- push_message.json.js
+++ push_message.json.js
@@ -3,4 +3,5 @@
 "content2":"<em style='color: gold;'>公告区：若Chrome访问YACD面板登录失败，请将chrome://flags/#block-insecure-private-network-requests设置关闭(disable)</em>",
 "content3":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>MerlinClash猫咪爬梯</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;帮助文档：<a href='https://mcreadme.gitbook.io/mc/' target='_blank'>MerlinClash Wiki</a></em>",
 "content4":"<em style='color: gold;'>奈飞小铺：<a href='https://ihezu.run/wb7hNF' target='_blank'>流媒体影音（Netflix、Disney+、Youtube、HBO等）账号合租一站式服务平台，95折优惠代码：nf325【通用】</a></em>"
+"content5":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tianshuhl.com/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】</a></marquee>"
 }

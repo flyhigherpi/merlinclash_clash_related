68dab1a649dbdc545edf09b32c9070328620f38a
vj23456 <53201527+vj23456@users.noreply.github.com>
2022-12-10 01:42:07 +0800

    Update push_message.json.js

--- push_message.json.js
+++ push_message.json.js
@@ -1,6 +1,6 @@
 {
 "content1":"<em style='color: gold;'>公告区：【KC版本】<a href='https://t.me/merlinclashcat/463' target='_blank'>MerlinClash 22.05.20&nbsp;-&nbsp;005</a>已发，建议及时更新</em>",
-"content2":"<em style='color: gold;'>公告区：若Chrome访问YACD面板登录失败，请将chrome://flags/#block-insecure-private-network-requests设置关闭(disable)</em>",
+"content2":"<em style='color: gold;'>公告区：若Chrome/Edge访问在线YACD/Razord面板登录失败，按照&nbsp;<a href='https://mcreadme.gitbook.io/mc/faq/faq1#q17' target='_blank'>FAQ17</a>&nbsp;设置您的浏览器</em>",
 "content3":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>MerlinClash猫咪爬梯</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;帮助文档：<a href='https://mcreadme.gitbook.io/mc/' target='_blank'>MerlinClash Wiki</a></em>",
 "content4":"<em style='color: gold;'>奈飞小铺：<a href='https://ihezu.run/wb7hNF' target='_blank'>流媒体影音（Netflix、Disney+、Youtube、HBO等）账号合租一站式服务平台，95折优惠代码：nf325【通用】</a></em>",
 "content5":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tshl.work/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】</a>9折优惠码：月付&nbsp;&nbsp;<em style='color: white;'>6qREHDLE</em>&nbsp;&nbsp;年付&nbsp;&nbsp;<em style='color: white;'>PR6MmaKA</em></marquee>"

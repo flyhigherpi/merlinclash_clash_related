801fa1c3b9832b30afaac879393a52e6b6959180
vj23456 <53201527+vj23456@users.noreply.github.com>
2023-11-16 12:59:34 +0800

    更新 push_message.json.js

--- push_message.json.js
+++ push_message.json.js
@@ -1,9 +1,7 @@
 {
 "content1":"<em style='color: gold;'>公告区：【KC版本】<a href='https://t.me/merlinclashcat/596' target='_blank'>MerlinClash 23.10.09</a>已发，建议及时更新</em>",
 "content2":"<em style='color: gold;'>公告区：若Chrome/Edge访问在线YACD/Razord面板登录失败，按照&nbsp;<a href='https://mcreadme.gitbook.io/mc/faq/faq1#q17' target='_blank'>FAQ17</a>&nbsp;设置您的浏览器</em>",
-"content3":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>MerlinClash猫咪爬梯</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;帮助文档：<a href='https://mcreadme.gitbook.io/mc/' target='_blank'>MerlinClash Wiki</a></em>",
+"content3":"<em style='color: gold;'>帮助文档：<a href='https://mcreadme.gitbook.io/mc/' target='_blank'>MerlinClash Wiki</a></em>",
 "content4":"<em style='color: gold;'>奈飞小铺：<a href='https://ihezu.run/wb7hNF' target='_blank'>流媒体影音（Netflix、Disney+、Youtube、HBO等）账号合租一站式服务平台，95折优惠代码：nf325【通用】</a></em>",
-"content5":"<em style='color: gold;'>精灵学院：<a href='https://check.riolu.icu/' target='_blank'>全中转隧道机场 / 多国线路资源 / 解锁流媒体, Tiktok, ChatGPT, 月付低至6元。</a></em>",
-"content6":"<em style='color: gold;'>Chatgpt对话服务，GPT4 Claude2等大模型以及Ai绘画任意使用！<a href='https://chat.aiplus.lol/login?invite_code=bc1ca4c1' target='_blank'>点击注册</a></em>",
-"content7":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tshl.work/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】</a>9折优惠码：月付&nbsp;&nbsp;<em style='color: white;'>6qREHDLE</em>&nbsp;&nbsp;年付&nbsp;&nbsp;<em style='color: white;'>PR6MmaKA</em></marquee>"
+"content5":"<em style='color: gold;'>Chatgpt对话服务，GPT4 Claude2等大模型以及Ai绘画任意使用！<a href='https://chat.aiplus.lol/login?invite_code=bc1ca4c1' target='_blank'>点击注册</a></em>",
 }

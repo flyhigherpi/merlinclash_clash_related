9619b44db4033aa2e4e2d4bc3f29cac3f195c07d
vj23456 <53201527+vj23456@users.noreply.github.com>
2023-11-23 11:28:35 +0800

    更新 push_message.json.js

--- push_message.json.js
+++ push_message.json.js
@@ -3,5 +3,6 @@
 "content2":"<em style='color: gold;'>公告区：若Chrome/Edge访问在线YACD/Razord面板登录失败，按照&nbsp;<a href='https://mcreadme.gitbook.io/mc/faq/faq1#q17' target='_blank'>FAQ17</a>&nbsp;设置您的浏览器</em>",
 "content3":"<em style='color: gold;'>帮助文档：<a href='https://mcreadme.gitbook.io/mc/' target='_blank'>MerlinClash Wiki</a></em>",
 "content4":"<em style='color: gold;'>奈飞小铺：<a href='https://ihezu.run/wb7hNF' target='_blank'>流媒体影音（Netflix、Disney+、Youtube、HBO等）账号合租一站式服务平台，95折优惠代码：nf325【通用】</a></em>",
-"content5":"<em style='color: gold;'>Chatgpt对话服务，GPT4 Claude2等大模型以及Ai绘画任意使用！<a href='https://chat.aiplus.lol/login?invite_code=bc1ca4c1' target='_blank'>点击注册</a></em>",
+"content5":"<em style='color: gold;'>精灵学院：<a href='https://info.riolu.icu' target='_blank'>全中转隧道机场 / 解锁流媒体,Tiktok,ChatGPT / 专属EMBY服务,月付低至6元。</a></em>",
+"content6":"<em style='color: gold;'>Chatgpt对话服务，GPT4 Claude2等大模型以及Ai绘画任意使用！<a href='https://chat.aiplus.lol/login?invite_code=bc1ca4c1' target='_blank'>点击注册</a></em>"
 }

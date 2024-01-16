737032c20294c748e97e25c2c2496306110d39ce
vj23456 <53201527+vj23456@users.noreply.github.com>
2024-01-16 09:59:45 +0800

    更新 push_message.json.js

--- push_message.json.js
+++ push_message.json.js
@@ -3,5 +3,6 @@
 "content2":"<em style='color: gold;'>公告区：若Chrome/Edge访问在线YACD/Razord面板登录失败，按照&nbsp;<a href='https://mcreadme.gitbook.io/mc/faq/faq1#q17' target='_blank'>FAQ17</a>&nbsp;设置您的浏览器</em>",
 "content3":"<em style='color: gold;'>帮助文档：<a href='https://mcreadme.gitbook.io/mc/' target='_blank'>MerlinClash Wiki</a></em>",
 "content4":"<em style='color: gold;'>奈飞小铺：<a href='https://ihezu.run/wb7hNF' target='_blank'>流媒体影音（Netflix、Disney+、Youtube、HBO等）账号合租一站式服务平台，95折优惠代码：nf325【通用】</a></em>",
-"content5":"<em style='color: gold;'>精灵学院：<a href='https://info.riolu.icu' target='_blank'>全中转隧道机场 / 解锁流媒体,Tiktok,ChatGPT / 专属EMBY服务,月付低至6元。</a></em>"
+"content5":"<em style='color: gold;'>精灵学院：<a href='https://info.riolu.icu' target='_blank'>全中转隧道机场 / 解锁流媒体,Tiktok,ChatGPT / 专属EMBY服务,月付低至6元。</a></em>",
+"content6":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tshl.work/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】</a>9折优惠码：月付&nbsp;&nbsp;<em style='color: white;'>6qREHDLE</em>&nbsp;&nbsp;年付&nbsp;&nbsp;<em style='color: white;'>PR6MmaKA</em></marquee>"
 }

d47246a9a404096d5c8fe196842c82d6dbfbfbf5
sadog <sadoneli@gmail.com>
2025-03-06 01:01:11 +0800

    Update push_message.json.js

--- push_message.json.js
+++ push_message.json.js
@@ -1,9 +1,8 @@
 {
-"content1":"<em style='color: gold;'>公告区：【KC版本】<a href='https://t.me/merlinclashcat/675' target='_blank'>MerlinClash 24.12.25</a>已发，建议及时更新</em>",
-"content2":"<em style='color: gold;'>公告区：若Chrome/Edge访问在线YACD/Razord面板登录失败，按照&nbsp;<a href='https://mcreadme.gitbook.io/mc/faq/faq1#q17' target='_blank'>FAQ17</a>&nbsp;设置您的浏览器</em>",
-"content3":"<em style='color: gold;'>帮助文档：<a href='https://mcreadme.gitbook.io/mc/' target='_blank'>MerlinClash Wiki</a></em>",
-"content4":"<em style='color: gold;'>奈飞小铺：<a href='https://ihezu.run/wb7hNF' target='_blank'>流媒体影音（Netflix、Disney+、Youtube、HBO等）账号合租一站式服务平台，95折优惠代码：nf325【通用】</a></em>",
-"content5":"<em style='color: gold;'>合作机场：<a href='https://98a6251b6cd7471da86cca993b6dbe6f.36d.biz/#/register?code=evoVRnbm' target='_blank'>全中转隧道机场 / 解锁流媒体 / 80+多国线路 / Tiktok / ChatGPT。9折优惠码: MerlinClash</a></em>",
+"content1":"<em style='color: gold;'>📢公告区1：【KC版本】<a href='https://t.me/merlinclashcat/675' target='_blank'>MerlinClash 24.12.25</a>已发，建议及时更新</em>",
+"content2":"<em style='color: gold;'>📢公告区2：若Chrome/Edge访问在线YACD/Razord面板登录失败，按照&nbsp;<a href='https://mcreadme.gitbook.io/mc/faq/faq1#q17' target='_blank'>FAQ17</a>&nbsp;设置您的浏览器</em>",
+"content3":"<em style='color: gold;'>📄帮助文档：<a href='https://mcreadme.gitbook.io/mc/' target='_blank'>MerlinClash Wiki</a></em>",
+"content4":"<em style='color: gold;'>📺️奈飞小铺：</em><a href='https://ihezu.run/wb7hNF' target='_blank'>流媒体影音（Netflix、Disney+、Youtube、HBO等）账号合租一站式服务平台，95折优惠代码：nf325【通用】</a>",
+"content5":"<em>🚀合作机场：<a href='https://98a6251b6cd7471da86cca993b6dbe6f.36d.biz/#/register?code=evoVRnbm' target='_blank'>全中转隧道机场 / 解锁流媒体 / 80+多国线路 / Tiktok / ChatGPT。9折优惠码: MerlinClash</a>",
 "content6":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tshl.win/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】</a>新春优惠码：年付&nbsp;&nbsp;<em style='color: white;'>jG813hJM</em></marquee>"
 }
-

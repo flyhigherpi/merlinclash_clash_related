c30080cb047f526b523962fb8e667e86cac579be
vj23456 <53201527+vj23456@users.noreply.github.com>
2025-03-06 09:42:45 +0800

    Update push_message.json.js

--- push_message.json.js
+++ push_message.json.js
@@ -1,7 +1,7 @@
 {
-"content1":"<em style='color: gold;'>📢公告区1：【KC版本】<a href='https://t.me/merlinclashcat/675' target='_blank'>MerlinClash 24.12.25</a>已发，建议及时更新</em>",
-"content2":"<em style='color: gold;'>📢公告区2：若Chrome/Edge访问在线YACD/Razord面板登录失败，按照&nbsp;<a href='https://mcreadme.gitbook.io/mc/faq/faq1#q17' target='_blank'>FAQ17</a>&nbsp;设置您的浏览器</em>",
-"content3":"<em style='color: gold;'>📄帮助文档：<a href='https://mcreadme.gitbook.io/mc/' target='_blank'>MerlinClash Wiki</a></em>",
+"content1":"<em style='color: gold;'>📢公告区1：<a href='https://t.me/merlinclashcat/704' target='_blank'>MerlinClash2 v0.2</a>已发，建议及时更新</em>",
+"content2":"<em style='color: gold;'>📄帮助文档：<a href='https://mcreadme.gitbook.io/mc/' target='_blank'>MerlinClash Wiki</a></em>",
+"content3":"<em style='color: gold;'>精灵学院：<a href='https://info.riolu.icu' target='_blank'>全中转隧道机场 / 解锁流媒体,Tiktok,ChatGPT / 专属EMBY服务,月付低至6元。</a></em>",
 "content5":"<em>🚀合作机场：</em><em style='color: gold;'>ssLinks</em><a href='https://98a6251b6cd7471da86cca993b6dbe6f.36d.biz/#/register?code=evoVRnbm' target='_blank'> | 性价比全中转机场 / 流媒体解锁 / 90+多国线路 / Tiktok / ChatGPT / udp | 专属9折优惠码：<em>MerlinClash</em></a>",
 "content6":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tshl.win/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】</a>新春优惠码：年付&nbsp;&nbsp;<em style='color: white;'>jG813hJM</em></marquee>"
 }

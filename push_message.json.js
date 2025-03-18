3919ae18aa26b8ef5e47d2be4e2aa69bb57ed350
sadog <sadoneli@gmail.com>
2025-03-18 22:30:58 +0800

    Update push_message.json.js

--- push_message.json.js
+++ push_message.json.js
@@ -2,6 +2,6 @@
 "content1":"<em style='color: gold;'>📢公告区1：<a href='https://t.me/merlinclashcat/704' target='_blank'>MerlinClash2 v0.2</a>已发，建议及时更新|📄帮助文档：<a href='https://mcreadme.gitbook.io/mc/' target='_blank'>MerlinClash Wiki</a></em>",
 "content2":"<em style='color: gold;'>🎬奈飞小铺：<a href='https://www.ihezu.cn/?sid=wb7hNF' target='_blank'>Netflix、Spotify、Youtube等上车了喂，89折优惠代码：nf325【通用】</a></em>",
 "content3":"<em style='color: gold;'>👻精灵学院：<a href='https://info.riolu.icu' target='_blank'>全中转隧道 / 解锁流媒体,Tiktok,ChatGPT / 专属EMBY服务,月付低至6元。</a></em>",
-"content4":"<em>🚀合作起飞：</em><em style='color: gold;'>ssLinks</em><a href='https://98a6251b6cd7471da86cca993b6dbe6f.36d.biz/#/register?code=evoVRnbm' target='_blank'> | 性价比全中转机场 / 流媒体解锁 / 90+多国线路 / Tiktok / ChatGPT / udp | 专属9折优惠码：<em>MerlinClash</em></a>",
+"content4":"<em>🚀合作起飞：</em><em style='color: gold;'>ssLinks</em><a href='https://98a6251b6cd7471da86cca993b6dbe6f.36d.biz/#/register?code=evoVRnbm' target='_blank'> | 全专线高速机场 / 流媒体解锁 / 90+多国线路 / Tiktok / ChatGPT / udp | 专属9折优惠码：<em>MerlinClash</em></a>",
 "content5":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tshl.win/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】</a>新春优惠码：年付&nbsp;&nbsp;<em style='color: white;'>jG813hJM</em></marquee>"
 }

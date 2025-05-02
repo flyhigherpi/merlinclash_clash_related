ca0cf52caf3c57b322ccba6e26c350b068893b81
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2025-05-02 13:51:10 +0800

    Update push_message.json.js

--- push_message.json.js
+++ push_message.json.js
@@ -4,5 +4,5 @@
 "content3":"<em style='color: gold;'>🎬银河录像厅：<a href='https://nf.video/hNFjm' target='_blank'>ChatGPT Plus、Netflix、Disney+、Spotify、YouTube等会员合租车，95折优惠代码：5203344</a></em>",
 "content4":"<em style='color: gold;'>👻精灵学院：<a href='https://info.riolu.icu' target='_blank'>全中转隧道 / 解锁流媒体,Tiktok,ChatGPT / 专属EMBY服务,月付低至6元。</a></em>",
 "content5":"<em>🚀合作起飞：</em><em style='color: gold;'>ssLinks</em><a href='https://98a6251b6cd7471da86cca993b6dbe6f.36d.biz/#/register?code=evoVRnbm' target='_blank'> | 全专线高速机场 / 流媒体解锁 / 90+多国线路 / Tiktok / ChatGPT / udp | 专属9折优惠码：<em>MerlinClash</em></a>",
-"content6":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tshl.win/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】</a>新春优惠码：年付&nbsp;&nbsp;<em style='color: white;'>jG813hJM</em></marquee>"
+"content6":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tshl.win/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】</a>五一限时优惠码：&nbsp;&nbsp;<em style='color: white;'>mbs2xkh9</em></marquee>"
 }

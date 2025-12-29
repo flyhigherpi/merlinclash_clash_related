702d9082ef41b699a3b466164757d5c5a760b3dd
vj23456 <53201527+vj23456@users.noreply.github.com>
2025-12-29 08:40:54 +0800

    Update push_message.json.js

--- push_message.json.js
+++ push_message.json.js
@@ -1,8 +1,7 @@
 {
-"content1":"<em style='color: gold;'>📢公告区1：<a href='https://t.me/merlinclashcat/714' target='_blank'>MerlinClash2 v0.4.2</a>已发，建议及时更新&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;📄帮助文档：<a href='https://mcreadme.gitbook.io/mc/' target='_blank'>MerlinClash Wiki</a></em>",
+"content1":"<em style='color: gold;'>📢公告区1：📄帮助文档：<a href='https://mcreadme.gitbook.io/mc/' target='_blank'>MerlinClash Wiki</a></em>",
 "content2":"<em style='color: gold;'>🎬奈飞小铺：<a href='https://www.ihezu.cn/?sid=wb7hNF' target='_blank'>Netflix、Spotify、Youtube等上车了喂，89折优惠代码：nf325【通用】</a></em>",
 "content3":"<em style='color: gold;'>🎬银河录像厅：<a href='https://nf.video/hNFjm' target='_blank'>ChatGPT Plus、Netflix、Disney+、Spotify、YouTube等会员合租车，95折优惠代码：5203344</a></em>",
-"content4":"<em style='color: gold;'>👻精灵学院：<a href='https://info.riolu.icu' target='_blank'>全中转隧道 / 解锁流媒体,Tiktok,ChatGPT / 专属EMBY服务,月付低至6元。</a></em>",
-"content5":"<em>🚀合作起飞：</em><em style='color: gold;'>ssLinks</em><a href='https://98a6251b6cd7471da86cca993b6dbe6f.36d.biz/#/register?code=evoVRnbm' target='_blank'> | 全专线高速机场 / 流媒体解锁 / 90+多国线路 / Tiktok / ChatGPT / udp | 专属9折优惠码：<em>MerlinClash</em></a>",
-"content6":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tshl.win/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】</a>五一限时优惠码：&nbsp;&nbsp;<em style='color: white;'>mbs2xkh9</em></marquee>"
+"content4":"<em>🚀合作起飞：</em><em style='color: gold;'>ssLinks</em><a href='https://98a6251b6cd7471da86cca993b6dbe6f.36d.biz/#/register?code=evoVRnbm' target='_blank'> | 全专线高速机场 / 流媒体解锁 / 90+多国线路 / Tiktok / ChatGPT / udp | 专属9折优惠码：<em>MerlinClash</em></a>",
+"content5":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tshl.win/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】</a>五一限时优惠码：&nbsp;&nbsp;<em style='color: white;'>mbs2xkh9</em></marquee>"
 }

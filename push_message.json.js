cac4e483ddf563f48a87b89d086ed548024c9f21
vj23456 <53201527+vj23456@users.noreply.github.com>
2026-05-11 14:10:07 +0800

    Update push_message.json.js

--- push_message.json.js
+++ push_message.json.js
@@ -3,5 +3,5 @@
 "content2":"<em style='color: gold;'>🎬奈飞小铺：<a href='https://ihezu.world/wb7hNF' target='_blank'>Netflix、Spotify、Youtube等上车了喂，89折优惠代码：nf325【通用】</a></em>",
 "content3":"<em style='color: gold;'>🎬银河录像厅：<a href='https://nf.video/hNFjm' target='_blank'>ChatGPT Plus、Netflix、Disney+、Spotify、YouTube等会员合租车，95折优惠代码：5203344</a></em>",
 "content4":"<em>🚀合作起飞：</em><em style='color: gold;'>ssLinks</em><a href='https://98a6251b6cd7471da86cca993b6dbe6f.36d.biz/#/register?code=evoVRnbm' target='_blank'> | 全专线高速机场 / 流媒体解锁 / 90+多国线路 / Tiktok / ChatGPT / udp | 专属9折优惠码：<em>MerlinClash</em></a>",
-"content5":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://tshl.win/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】</a>五一限时优惠码：&nbsp;&nbsp;<em style='color: white;'>mbs2xkh9</em></marquee>"
+"content5":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://778007.xyz/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【天枢互联】</a>五一限时优惠码：&nbsp;&nbsp;<em style='color: white;'>mbs2xkh9</em></marquee>"
 }
